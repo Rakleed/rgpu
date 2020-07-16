eeff46115ba558825432af5bc859e8b23be8a772
Pavel Moiseenko <19418601+Rakleed@users.noreply.github.com>
2020-07-16 15:57:03 +0300

    Add indepworkinvar3-3

---
+++ 3-semester/programming/indepworkinvar3-2.py
@@ -0,0 +1,34 @@
+"""
+    Автор: Моисеенко Павел, группа № 1, подгруппа № 2.
+
+    ИСР 3.2. Задание: разработайте сценарий с реализацией операции поиска подстроки в тексте.
+
+"""
+
+
+def main():
+    search_string = input("Введите строку. ")
+    what_to_find = input("Что бы вы хотели найти? ")
+    all_occurrences(search_string, what_to_find)
+
+
+def all_occurrences(search_string, what_to_find):
+    occurrence = search_string.find(what_to_find)
+    if occurrence != -1:
+        print('Первое вхождение подстроки:', occurrence)
+    else:
+        print('Нет вхождений подстроки.')
+
+    length_search_string = len(search_string)
+    length_what_to_find = len(what_to_find)
+    while occurrence != -1:
+        occurrence = search_string.find(what_to_find, occurrence +
+                                        length_what_to_find,
+                                        length_search_string)
+        if occurrence != -1:
+            print('Вхождение подстроки:', occurrence)
+        else:
+            print('Больше вхождений нет.')
+
+
+main()
