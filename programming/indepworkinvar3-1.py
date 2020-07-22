80b511247f13bc4f0a67d32be55c757cd5eb969c
Pavel Moiseenko <19418601+Rakleed@users.noreply.github.com>
2020-07-22 22:50:26 +0300

    Update indepworkinvar3-1.py

--- programming/indepworkinvar3-1.py
+++ programming/indepworkinvar3-1.py
@@ -12,8 +12,16 @@ class Post:
         self.__author = author
         self.__post_text = post_text
 
+    def show_comment(self, comment=object):
+        print(f'Никнейм: "{comment.nickname}", текст комментария: "{comment.comment_text}".')
+
 
 class Comment(Post):
     def __init__(self, nickname, comment_text):
-        self.__nickname = nickname
-        self.__comment_text = comment_text
+        self.nickname = nickname
+        self.comment_text = comment_text
+
+
+post_test = Post("Иван Александров", "Какая-то интересная статья.")
+comment_test = Comment("var_inb2", "Great job!")
+post_test.show_comment(comment=comment_test)
