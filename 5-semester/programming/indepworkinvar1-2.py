71dfe0d50ff3dae768e2103c9a13b829140f0521
Pavel Moiseenko <19418601+rakleed@users.noreply.github.com>
2021-06-28 11:28:26 +0300

    Create indepworkinvar1-2.py

---
+++ 5-semester/programming/indepworkinvar1-2.py
@@ -0,0 +1,33 @@
+"""
+    Автор: Моисеенко Павел, подгруппа № 2.
+    ИСР 1.2. Задание: создать простейший стенарий по выводу текста на экран.
+
+"""
+
+import json
+import os
+
+
+class Guestbook:
+    def create_json(self, filename='guestbook.json'):
+        with open(filename, 'w') as file:
+            file.write('')
+
+    def remove_json(self, filename='guestbook.json'):
+        file_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), filename)
+        os.remove(file_path)
+
+    def rename_json(self, new_filename, filename='guestbook.json'):
+        os.rename(filename, new_filename)
+
+    def get_json(self, filename='guestbook.json'):
+        with open(filename, 'r') as file:
+            read_json = json.load(file)
+        return read_json
+
+    def write_json(self, json_text, filename='guestbook.json'):
+        with open(filename, 'w') as file:
+            json.dump(json_text, file, ensure_ascii=False, indent=4)
+
+# Guestbook.create_json()
+# GuestBook.remove_json('data_backup.json')
