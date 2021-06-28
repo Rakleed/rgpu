ee8b268c042b7a66125229a9f82d849e90fc2950
Pavel Moiseenko <19418601+rakleed@users.noreply.github.com>
2021-06-28 19:40:40 +0300

    Create indepworkinvar2-1.py

---
+++ 5-semester/programming/indepworkinvar2-1.py
@@ -0,0 +1,20 @@
+"""
+    Автор: Моисеенко Павел, подгруппа № 2.
+    ИСР 2.1. Задание: разработать функцию, возвращающую элементы ряда
+    Фибоначчи по данному максимальному значению.
+
+"""
+
+
+def fibonacci_max(max_number):
+    fibonacci_list = [0, 1]
+    current_fibonacci = fibonacci_list[-1]
+    while current_fibonacci <= max_number:
+        fibonacci_list += [fibonacci_list[-2] + fibonacci_list[-1]]
+        current_fibonacci = fibonacci_list[-1]
+    fibonacci_list.pop(-1)
+    return fibonacci_list
+
+
+print(fibonacci_max(88))
+print(fibonacci_max(89))
