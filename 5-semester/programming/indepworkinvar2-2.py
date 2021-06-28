2f59faedaa0add5a4bbe297cb84b3df36976d858
Pavel Moiseenko <19418601+rakleed@users.noreply.github.com>
2021-06-28 19:48:24 +0300

    Create indepworkinvar2-2.py

---
+++ 5-semester/programming/indepworkinvar2-2.py
@@ -0,0 +1,20 @@
+"""
+    Автор: Моисеенко Павел, подгруппа № 2.
+    ИСР 2.2. Задание: создать программу, возвращающую список чисел
+    Фибоначчи с помощью итератора.
+
+"""
+
+
+def fibonacci_max(max_number):
+    fibonacci_list = [0, 1]
+    current_fibonacci = fibonacci_list[-1]
+    while current_fibonacci <= max_number:
+        fibonacci_list += [fibonacci_list[-2] + fibonacci_list[-1]]
+        current_fibonacci = fibonacci_list[-1]
+    fibonacci_list.pop(-1)
+    return fibonacci_list
+
+
+for i in fibonacci_max(90):
+    print(i)
